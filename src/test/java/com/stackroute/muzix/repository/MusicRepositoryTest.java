05c3c42f1266090a8ecc3e647b74066f848b763d
22Saranya <saraarumugam22@gmail.com>
2019-05-27 12:09:27 +0530

    Test cases done for service

---
+++ src/test/java/com/stackroute/muzix/repository/MusicRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.stackroute.muzix.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MusicRepositoryTest {
+
+    @Test
+    public void getByName() {
+    }
+}
